63eeaddc03fc82496541e11ed3c5595b47cccbc0
danilbakshaev <bakshaevdanil@gmail.com>
2020-12-28 19:43:44 +0500

    Закончил модалку, добавил карточки

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const modal = $.modal()
+const emoji = [
+  {id: 1, title: 'Мощь', price: 20, emoji: '💪'},
+  {id: 2, title: 'Сердце', price: 30, emoji: '❤️'},
+  {id: 3, title: 'Мозг', price: 40, emoji: '🧠'}
+]
+
+const modal = $.modal({
+  title: 'Danil modal',
+  closable: true,
+  content: `
+    <p>lorem ipsum dolor sit.</p>
+    <p>lorem ipsum dolor sit.</p>
+  `,
+  width: '400px',
+  footerButtons: [
+    {text: 'Ок', type: 'primary', handler() {
+      console.log('Primary btn clicked')
+      modal.close()
+    }},
+    {text: 'Cancel', type: 'danger', handler() {
+      console.log('Danger btn clicked')
+      modal.close()
+    }},
+  ]
+})
